df33d514753d6e96f2c9150489faf7cb6c3bf400
Your Name <barron.luccioni@gmail.com>
2021-09-07 12:48:33 -0500

    fix: dashboard ws

--- src/components/_dashboard/app/AppWebsiteVisits.js
+++ src/components/_dashboard/app/AppWebsiteVisits.js
@@ -10,82 +10,83 @@ import { getToken } from 'src/services/tokens';
 import { wsRoute } from 'src/services/constants';
 // ----------------------------------------------------------------------
 
-
-let websocket = !isNull(getToken()) ? new WebSocket(wsRoute + '?Auth=' + getToken()) : '';
-
-let LABEL_DATA = [];
-let GRAPHIC_DATA = [ {data: []} ];
-
 export default function AppWebsiteVisits({ device, llave }) {
+  const [ws] = useState(new WebSocket(wsRoute + '?Auth=' + getToken()));
+  const [wsOpen, setWsOpen] = useState(false);
+  const [data, setData] = useState([]);
+  const [labels, setLabels] = useState([]);
+
   let titleGraphic = `Sensor de ${device.grupo.toLowerCase()}`;
   let subtitleGraphic = `Medida de ${device.grupo.toLowerCase()} en ${device.unidad_medida.toLowerCase()}`;
 
-  if(!isNull(getToken()) ) { 
-    if(typeof websocket === 'string') { 
-      websocket =  new WebSocket(wsRoute + '?Auth=' + getToken());
+  useEffect(() => {
+    let websocket = ws;
+    if(!isNull(getToken()) ) { 
+      websocket.addEventListener('open', () => {
+        console.log('Websocket is connected');
+    
+        const data = {
+          action: 'listenDevice',
+          deviceId: device.deviceId,
+          dataHistory: 'true',
+          timeHistory: '24'
+        }
+    
+        websocket.send(JSON.stringify(data));
+        setWsOpen(true);
+      });
+  
+      websocket.addEventListener('close', () => {
+        console.log('Websocket is closed');
+      });
+    
+      websocket.addEventListener('error', (e) => console.log('Websocket is in error', e));
     }
-
-    websocket.addEventListener('open', () => {
-      console.log('Websocket is connected');
-  
-      const data = {
-        action: 'listenDevice',
-        deviceId: device.deviceId,
-        dataHistory: 'true',
-        timeHistory: '24'
-      }
-  
-      websocket.send(JSON.stringify(data));
-    });
-
-    websocket.addEventListener('close', () => {
-      console.log('Websocket is closed');
-    });
-  
-    websocket.addEventListener('error', (e) => console.log('Websocket is in error', e));
-  }
-
-  const [data, setData] = useState([]);
+  }, []);
 
   useEffect(() => {
-  if(!isNull(getToken()) ) { 
-    websocket.addEventListener('message', (e) => {
-      console.log('Message received', JSON.parse(e.data));
-      const response = JSON.parse(e.data);
+    if(!wsOpen) return;
+    let websocket = ws;
+    if(!isNull(getToken()) ) { 
+      websocket.addEventListener('message', (e) => {
+        console.log('Message received', JSON.parse(e.data));
+        const response = JSON.parse(e.data);
+        let valuesArr = [];
+        let labelsArr = [];
 
 
-
-      if(response.hasOwnProperty('message')) {
-        const { devices } = response;
-        for(let item of devices) { 
-          if(item.deviceId == device.deviceId) {
-            const value = Number(item.v1);
-
-            GRAPHIC_DATA[0].data.push(value)
-            LABEL_DATA.push(item.ts)
+        if(response.hasOwnProperty('message')) {
+          const { devices } = response;
+          for(let item of devices) { 
+            if(item.deviceId == device.deviceId) {
+              const value = Number(item.v1);
+              valuesArr.push(value);
+              labelsArr.push(item.ts)
+            }
+          }
+        } else { 
+          if(response.deviceId == device.deviceId) {
+            const value = Number(response.v1);
+            valuesArr.push(value);
+            labelsArr.push(response.ts)
           }
         }
-      } else { 
-        if(response.deviceId == device.deviceId) {
-          const value = Number(response.v1);
 
-          GRAPHIC_DATA[0].data.push(value)
-
-          LABEL_DATA.push(response.ts)
-        }
-      }
-
-
-      setData(() => [...GRAPHIC_DATA] );
-    });
-  }
-  }, []);
+        setData([
+          ...valuesArr
+        ]);
+        setLabels([
+          ...labelsArr
+        ]);
+      });
+    }
+  }, [wsOpen]);
 
   const chartOptions = merge(BaseOptionChart(), {
     chart: { animations: { enabled: false } },
     plotOptions: { bar: { columnWidth: '11%', borderRadius: 4 } },
     fill: { type: ['solid', 'gradient', 'solid'] },
-    labels: LABEL_DATA,
+    labels,
     tooltip: {
       shared: true,
       intersect: false,
@@ -115,7 +116,7 @@ export default function AppWebsiteVisits({ device, llave }) {
     <Card>
       <CardHeader title={titleGraphic} subheader={subtitleGraphic} />
       <Box sx={{ p: 3, pb: 1 }} dir="ltr">
-        <ReactApexChart type="line" series={data} options={chartOptions} height={364} />
+        <ReactApexChart type="line" series={[{data}]} options={chartOptions} height={364} />
       </Box>
     </Card>
   );
