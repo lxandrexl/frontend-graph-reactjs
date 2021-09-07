f6a11ec586a4f6748873136c020cd6ceec1a2c47
Joshua Villena Diaz <jovillenad@gmail.com>
2021-09-07 00:34:43 -0500

    .

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,5 +1,4 @@
 export const clientId = process.env.REACT_APP_CLIENTID;
 export const userPoolId = process.env.REACT_APP_USERPOOLID;
-export const wsRoute = 'wss://4ddl79toe6.execute-api.us-east-1.amazonaws.com/DESA';
 export const wsRoute = 'wss://yhkt198qze.execute-api.us-east-1.amazonaws.com/TEST';
 export const baseUrl = 'https://8nu9yz5794.execute-api.us-east-1.amazonaws.com/TEST';
